a7a07b9c526e8e887c3bda0b6626175517cbc05d
Piotr Stankowski <git@trakos.pl>
2016-04-26 22:21:42 +0200

    Updated netfan repositories

--- CFAN-netfan/CfanAggregator/FactorioModsCom/FmmConverter/FmmIgnorer.cs
+++ CFAN-netfan/CfanAggregator/FactorioModsCom/FmmConverter/FmmIgnorer.cs
@@ -18,6 +18,8 @@ namespace CFAN_netfan.CfanAggregator.FactorioModsCom.FmmConverter
             "dytech", // two mods and two cfan.jsons crammed into one zip file - maybe should handle it and split them
             "ScienceCostTweaker",
             "marathon",
+            "More_Power_Factorio", "Power lines will now spawn between electricity using items", // scam
+            "sleaf-Tower-Defense", // scenario with zip in rar
         };
 
         public IEnumerable<CfanJson> generateCfanJsons(IUser user, ModJson modJson)
