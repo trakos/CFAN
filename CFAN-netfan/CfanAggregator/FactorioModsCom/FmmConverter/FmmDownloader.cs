5f88f46b9bc04a1d8f86140134eb997e97f0943c
Piotr Stankowski <git@trakos.pl>
2016-06-19 19:12:57 +0200

    Netfan: catching mod normalization exceptions in.

--- CFAN-netfan/CfanAggregator/FactorioModsCom/FmmConverter/FmmDownloader.cs
+++ CFAN-netfan/CfanAggregator/FactorioModsCom/FmmConverter/FmmDownloader.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using CFAN_netfan.CfanAggregator.Aggregators;
@@ -54,6 +55,11 @@ namespace CFAN_netfan.CfanAggregator.FactorioModsCom.FmmConverter
                     catch (NetfanDownloadKraken e)
                     {
                         user.RaiseError($"Couldn't download {modJson.name}: {e.Message}");
+                        continue;
+                    }
+                    catch (Exception e)
+                    {
+                        user.RaiseError($"Couldn't handle {modJson.name}: {e.Message}");
                         continue;
                     }
                     yield return fmmManager.generateCfanFromZipFile(user, downloadedFilePath, new Dictionary<string, string>
